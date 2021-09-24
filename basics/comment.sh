3bf18422702d06ae0c3e000d4b54cc231154c18d
FahimMuntashir <fahim.tech.me@gmail.com>
2021-09-25 02:52:57 +0600

    added ...

---
+++ basics/comment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# multiple line comment
+<< comment
+echo "a"
+echo "b"
+echo "c"
+
+comment
---
+++ basics/var_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+x=5
+y=10
+
+echo "$x"
+echo "$y"
+
+# we can use double quote or no quote with dollar sign but if we use single quote then it will print as it is
+
+<< comment
+echo "a"
+echo "b"
+echo "c"
+
+comment
