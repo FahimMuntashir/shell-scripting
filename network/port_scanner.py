12fb0a116df99bf044b5f50f58e036e744dbfcfd
FahimMuntashir <fahim.tech.me@gmail.com>
2022-01-01 23:48:35 +0600

    added...

--- network/port_scanner.py
+++ network/port_scanner.py
@@ -20,11 +20,11 @@ print("-"*50)
 
 
 try:
-        for port in range(1,100):
+        for port in range(50,85):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket.setdefaulttimeout(1)
             result  = s.connect_ex((target, port))
-            # print("checking port {}".format(port))
+            print("checking port {}".format(port))
             if result == 0:
                 print("Port {} is open.".format(port))
             s.close()
